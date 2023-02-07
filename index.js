cc80190cb77f6e32cb1f5f13cb58611dacdb1cf4
Mateo <mateo.orolot@gmail.com>
2023-02-06 22:14:24 -0500

    Create express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Server running on port ${PORT}`);
+});
+
+app.listen(PORT);
